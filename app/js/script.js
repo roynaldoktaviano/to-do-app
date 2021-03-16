06f7d82f24e0ca095e6e0552d9f792f303202f74
Roynald Oktaviano <roynald.okta@gmail.com>
2021-03-16 11:41:08 +0700

    Dark Mode Intiation

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
+const body = document.querySelector('body');
 const input = document.querySelector('#todo');
 const label = document.querySelector('.label label');
+const icon = document.querySelector('.icon-img');
 
+// Input Text
 if(!input.value) {
     input.addEventListener("focus",function(){
         label.classList.add('none');
@@ -10,3 +13,13 @@ if(!input.value) {
         label.classList.remove('none');
     })
 }
+
+// Dark Mode
+icon.addEventListener('click',function(){
+    body.classList.toggle('dark-mode');
+    if(body.classList.contains('dark-mode')){
+        icon.src = "/images/icon-sun.svg"
+    } else {
+        icon.src = "/images/icon-moon.svg"
+    }
+})
